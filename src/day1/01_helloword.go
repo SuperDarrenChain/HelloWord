84996a91bf91a87f316c4ac2280c7dde02322991
lxy <39685413+renyuanhao@users.noreply.github.com>
2018-11-01 10:32:52 +0800

    go

---
+++ src/day1/01_helloword.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+		fmt.Println("Helloword!!!")
+		fmt.Println("Helloword!!!")
+
+		var name int
+		//var ago int
+
+
+   fmt.Printf("%T,%v",name,name)
+
+
+
+}
